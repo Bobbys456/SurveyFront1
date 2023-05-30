da4890c60cc2edeab321747caee44091d56b3df0
Bobby Sullivan <bobbysullivan61@gmail.com>
2023-05-30 19:39:39 -0400

    added question page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import * as React from 'react';
+
+import React, { useEffect, useState } from "react";
 import './index.css';
 import LoginPage from './login.js';
 import SurveyPage from './survey';
+import axios from 'axios';
+
+
 
 export default function MyApp() {
 
@@ -11,17 +15,71 @@ export default function MyApp() {
 
   if(!verified){
     return (
-    
-      <LoginPage error={error} valid={valid} verified={verified}></LoginPage>
+      <>
+        <LoginPage error={error} valid={valid} verified={verified} loginClick={loginClick}></LoginPage>
+      </>
+      
    
-  );
+    );
   } else {
     return (
     
       <SurveyPage></SurveyPage>
 
    
-  );
+    );
   }
   
+
+
+
+
+
+
+
+  
+ //this error methods sets the state to inclue error
+  function logError(i){
+    setError(i);
+    setValid(false);
+  }
+
+  //if email is valid and verified allows user to access survey page 
+  function validate(){
+    setVerify(true)
+  }
+
+  //handler method ofr when the verify email button is clicked. Makes sure email syntax is correct on client and check emails for double use at the server
+  function loginClick(emailInput){
+
+    const emailValue = emailInput.value;
+    
+    
+
+    if(emailValue.indexOf('@') < 0 || emailValue.indexOf('.') < 0 || emailValue.indexOf(' ') >= 0){
+    
+      logError('Not a valid email address.')
+    
+    }else{
+
+      axios.get('http://localhost:5000/', {
+          params: {
+            email: emailValue
+          }
+          }
+      ).then((response)=>{
+        
+        if(response.data === false){
+            logError('This email has already been used.'); 
+        }else{
+            validate(); 
+        }
+        console.log(response.data);
+      
+      }).catch(function (error) {
+        console.log(error);
+      })
+ 
+    }  
+  }
 }
--- src/survey.js
+++ src/survey.js
@@ -1,13 +1,78 @@
 import React, { useState } from 'react';
+import './index.css';
+import './survey.css';
 
 export default function Page() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
+
+    const questions = [
+      '1',
+      '2',
+      '3',
+      '4',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'Would you recommend our service to others?',
+      'What features would you like to see in future updates?',
+      'How would you rate the overall user experience?',
+      'last q \n bruh <br>sdkjfskadjf'
+    ];
+    
     
     return (
         <div id="box">
-        you did it! 
+          
+          // Spacer
+          <h2></h2>
+
+          <div class='sQuestionContainer'>
+            {questions.map((question,index) => (
+            <Question q={question} id={"q"+index}></Question>
+            ))}
+          </div>
+        
         </div>
       )
 
+}
+
+function Question(props){
+  return(
+    <div class='padder'>
+      <div class='sQuestionOutline'>
+        {props.q}
+        {"    " + props.id}
+        <input class='texAnswer' id={props.id} type= "text"></input>
+      </div>
+    </div>
+  )
 }
